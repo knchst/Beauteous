a70b07b55f72f66462266ae7cd6f05b71d2b1161
Kenichi Saito <tkcfjips@gmail.com>
2015-07-28 19:40:09 +0900

    Add project

---
+++ Beauteous/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Beauteous
+//
+//  Created by Kenichi Saito on 7/28/15.
+//  Copyright (c) 2015 Kenichi Saito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
